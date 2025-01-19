2fdd6df3db666e132587009c87e92dcc2d8e33af
artydev <salve.didio@laposte.net>
2025-01-19 16:39:21 +0100

    update navbar

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -1,25 +1,35 @@
 export default function Nav() {
     return (
-        <div>
+        <>
             <nav class="navbar">
-                <div class="navbar-left">
-                    <img src="/logo.png" width="80%" style="position:relative;top:0.1rem;"/>
-                </div>
+                <div class="navbar-top">
 
-                <div style="flex:1;background:red;text-align:center">
-                    <a href="#home">Home</a>
-                </div>
+                    <div class="logo">
+                        <img src="/logo.png" />
+                    </div>
 
-                <div class="navbar-right" style="text-align:right">
-                    RIGHT
-                </div>
+                    <div class="navbar-search">
+                        <a href="#home">Home</a>
+                    </div>
 
-                <div class="hamburger" id="hamburger">
-                    <span class="bar"></span>
-                    <span class="bar"></span>
-                    <span class="bar"></span>
+                    <div class="navbar-right">
+                        A Proposito
+                    </div>
+                </div>
+                <div class="navbar-bottom">
+                    <a href="#prodotti">Prodotti</a>
+                    <a href="#promozioni">Promozioni</a>
+                    <a href="#assistenza">Assistenza</a>
+                    <a href="#contattaci">Contattaci</a>
                 </div>
             </nav>
-        </div>
+
+            <div class="hamburger" id="hamburger">
+                <span class="bar"></span>
+                <span class="bar"></span>
+                <span class="bar"></span>
+            </div>
+
+        </>
     );
 }
