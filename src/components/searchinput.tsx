961e3508ed9b96102fbb8b71084115750869fd70
artydev <salve.didio@laposte.net>
2025-01-19 17:51:14 +0100

    Add magnify button

--- src/components/searchinput.tsx
+++ src/components/searchinput.tsx
@@ -3,7 +3,7 @@ import SearchButton  from "./search-button.tsx"
 export default function SearchInput() {
     return (
       <div style="display:flex; background:white;padding:0.5rem;border-radius:30px;height:3rem">
-            <input />
+            <input placeholder="Cosa cerchi?"/>
             <SearchButton />
       </div>
     );
