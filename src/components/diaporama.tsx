270256d4512c7e8f076cd39d11c3e690b7700bdb
artydev <salve.didio@laposte.net>
2025-01-19 22:19:01 +0100

    add background images

--- src/components/diaporama.tsx
+++ src/components/diaporama.tsx
@@ -2,22 +2,22 @@
 
 export default function Diaporama() {
     return (
-        <div class="swiper" style="background:#007A36">
+        <div class="swiper"  >
             <div class="swiper-wrapper" >
 
-                <div class="swiper-slide">
+                <div class="swiper-slide" style="background:#FF3131">
                     <img src="1-1.webp" />
                 </div>
-                <div class="swiper-slide">
-                    <img src="2.webp" />
+                <div class="swiper-slide" style="background:#8B54FE" >
+                    <img src="2.webp"  />
                 </div>
-                <div class="swiper-slide">
+                <div class="swiper-slide" style="background:#FE5758" >
                     <img src="3.webp" />
                 </div>
-                <div class="swiper-slide">
+                <div class="swiper-slide" style="background:#FFDD59" >
                     <img src="4.webp" />
                 </div>
-                <div class="swiper-slide">
+                <div class="swiper-slide" style="background:#0097B1" >
                     <img src="5.webp" />
                 </div>
             </div>
